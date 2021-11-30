39a5ccbe1fe1ee45bd8e33021f03d797860d1217
Junwen-Zhong735 <94938529+Junwen-Zhong735@users.noreply.github.com>
2021-11-30 06:05:32 -0800

    Update quiz 1 #3.asm

--- quiz 1 #3.asm
+++ quiz 1 #3.asm
@@ -20,9 +20,37 @@ main:
 	syscall 
 	
 	li	$v0, 6				# cin >> n 
-	mov.s	$f1, $f0 		# $f1 = n 
 	
-	li	$f2, 1				# $f2 = x = 1
+	li	$f1, 1				# $f1 = x = 1
+	li 	$f2, 2				# $f2 = 2
+	li	$f3, 0.00001			# $f3 = 0.00001
 	
-while: 
+loop: 
+	div.s	$f4, $f0, $f1			# $f4 = n/x
+	nop 
+	nop 
+	add.s	$f4,$f0, $f4 			# $f4 = x + n/x
+	div.s	$f4, $f4, $f2			# $f4 = (x + n/x)/2 = x'
+	nop 
+	nop 
+	sub.s 	$f5, $f4, $f1			# $f5 = x' - x
+	abs.s 	$f5, $f5			# $f5 = | x' - x |
+	c.lt.s	$f5, $f3			# | x' - x | < 0.00001
+	bf1f.s	loop 
+	nop 
+	
+exit: 
+	la	$a0, str2			# cout << str2
+	li 	$v0, 4
+	syscall
+	
+	mov.s 	$f12, $f4			# cout << x'
+	li 	$v0, 6 
+	syscall
+	
+	li $v0, 10 
+	syscall 
+	
+	
+	
 	
