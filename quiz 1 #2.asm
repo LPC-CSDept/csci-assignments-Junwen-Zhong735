c90fba765cf262367284721a058ac9ac61d0d477
Junwen-Zhong735 <94938529+Junwen-Zhong735@users.noreply.github.com>
2021-11-30 03:21:19 -0800

    Update quiz 1 #2.asm

--- quiz 1 #2.asm
+++ quiz 1 #2.asm
@@ -17,37 +17,48 @@ str5:	.asciiz, "ax^2 + bx + c = "
 	.globl main 
 	
 main: 
-	la	$a0, str1			# cout << str1;
+	la	$a0, str1			# cout << str1
 	li	$v0, 4
 	syscall 
 	
-	li	$v0, 5				# cin >> $f1; 
-	mov.s	$f0, $f1 		# $f1 = x
+	li	$v0, 5				# cin >> $f1 
+	mov.s	$f0, $f1 			# $f1 = x
 	
-	la	$a0, str2			# cout << str2;
+	la	$a0, str2			# cout << str2
 	li	$v0, 4
 	syscall 
 	
-	li	$v0, 5				# cin >> $f2; 
-	mov.s	$f0, $f2  		# $f2 = a
+	li	$v0, 5				# cin >> $f2 
+	mov.s	$f0, $f2  			# $f2 = a
 	
-	la	$a0, str3			# cout << str3;
+	la	$a0, str3			# cout << str3
 	li	$v0, 4
 	syscall 
 	
-	li	$v0, 5				# cin >> $f3; 
-	mov.s	$f0, $f3  		# $f3 = b
+	li	$v0, 5				# cin >> $f3 
+	mov.s	$f0, $f3  			# $f3 = b
 
-	la	$a0, str4			# cout << str4;
+	la	$a0, str4			# cout << str4
 	li	$v0, 4
 	syscall 
 	
-	li	$v0, 5				# cin >> $f4; 
-	mov.s	$f0, $f4  		# $f4 = c
+	li	$v0, 5				# cin >> $f4
+	mov.s	$f0, $f4  			# $f4 = c
 	
+	mul.s	$f0, $f1, $f2			# $f0 = ax
+	add.s	$f0, $f0, $f3			# $f0 = ax + b
+	mul.s	$f0, $f0, $f1			# $f0 = ax^2 + bx
+	add.s	$f0, $f0, $f4			# $f0 = ax^2 + bx + c
 	
+	la	$a0, str5			# cout << str5
+	li 	$v0, 4				
+	syscall 
 	
+	li	$v0, 2				# cout << ax^2 + bx + c
+	mov.s	$f12, $f0
 	
+	li	$v0, 10
+	syscall 
 	
 	
 	
