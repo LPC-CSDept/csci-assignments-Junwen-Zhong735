6d986acd129d479b824f15b47cf885ec60739c3b
Junwen-Zhong735 <94938529+Junwen-Zhong735@users.noreply.github.com>
2021-11-30 01:59:24 -0800

    Update quiz 1 #1.asm

--- quiz 1 #1.asm
+++ quiz 1 #1.asm
@@ -16,9 +16,30 @@ str2:	.asciiz, "\n"
 	.globl main
 	
 main: 
-	la $a0, prompt1			# print prompt for tempeture
-	li $v0, 4
+	la	$a0, prompt1			# cout << prompt1;
+	li	$v0, 4		
 	syscall 
 	
-	li $v0, 5				# $f0 = temp
+	li	$v0, 5					# cin >> $f0;
+	syscall
+	mtc1	$v0, $f0
+	cvt.s.w	$f0, $f0 
 	
+	li.s	$f1, 5.0			# $f1 = 5/9;
+	li.s	$f2, 9.0			
+	div.s	$f1, $f1, $f2
+	
+	li.s	$f2, 32.0			# $f2 = $f0 - 32;
+	sub.s 	$f2, $f0, $f2
+	
+	mul.s 	$f12, $f1, $f2 		# $f12 = $f1 * $f2;
+	
+	la	$a0, str2				# cout << str2;
+	li	$v0, 4 
+	syscall 
+	
+	li	$v0, 2					# cout << $f12;
+	syscall 
+	
+	li	$v0, 10
+	syscall 
