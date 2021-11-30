f80da8fcff7ba05e8a873182dcb8a383f2f44d69
Junwen-Zhong735 <94938529+Junwen-Zhong735@users.noreply.github.com>
2021-11-30 03:09:10 -0800

    Add files via upload

--- quiz 1 #2.asm
+++ quiz 1 #2.asm
@@ -1,4 +1,25 @@
 
+# 
+# Quiz 1 #2
+# Junwen Zhong
+# 11/23/21
+# calculate ax^2 + bx + c 
+#
+
+	.data 
+str1:	.asciiz, "Enter a number for x: "
+str2:	.asciiz, "Enter a number for a: "
+str3:	.asciiz, "Enter a number for b: "
+str4:	.asciiz, "Enter a number for c: "
+str5:	.asciiz, "ax^2 + bx + c = "
+	
+	.text
+	.globl main 
+	
+main: 
+	
+	
+	
 	
 	
 	
