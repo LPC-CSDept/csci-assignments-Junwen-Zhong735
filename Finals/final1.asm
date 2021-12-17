0dc71326111f47b3b88eda436f7cf8d380200015
Junwen-Zhong735 <94938529+Junwen-Zhong735@users.noreply.github.com>
2021-12-17 13:58:32 -0800

    $a0 to hold sum instead of $s2

--- Finals/final1.asm
+++ Finals/final1.asm
@@ -14,7 +14,7 @@ waiting:
 	beqz	$t1, waiting		# loop if LSB is 0
 
     lw	$s1, 4($s0)				# read from input device if LSB = 1
-	sub $s1, $s1, 48			# $s1 = $v0 -48
+	sub $s1, $s1, 48			# $s1 = input -48
 	
 	sub	$t0,$t0, 1				# increment counter
 	beqz 	$t0,  transmit		# end loop after 3 digits 
@@ -26,18 +26,18 @@ waiting:
 
     li  $t1, 100                # multiply by 100 for 1st digit 
     mult	$s1, $t1 
-    mflo    $s2 
+    mflo    $a0 
 
 digit2: 
 	li 	$t1, 10 				# multiply by 10 for 2nd digit 
 	mult	$s1, $t1 
 	mflo	$t1 
-	add 	$s2, $s2, $t1		# combine 1st digit and 2nd digit 
+	add 	$a0, $a0, $t1		# combine 1st digit and 2nd digit 
 
     j waiting 					# continue loop 
 
 transmit: 
-    add 	$a0, $s2, $s1 		# combine all 3 digits 
+    add 	$a0, $a0, $s1 		# combine all 3 digits 
 	li 	$v0, 1 					# print decimal (integer) 
     syscall
 
