eeee2ae5df06661ea9e3cf0f1d0716015f541b71
Junwen-Zhong735 <94938529+Junwen-Zhong735@users.noreply.github.com>
2021-12-17 13:51:09 -0800

    update, added missing syscalls

--- Finals/final1.asm
+++ Finals/final1.asm
@@ -21,6 +21,9 @@ waiting:
 
     j waiting 					# continue loop 
 	
+    li	$t1, 1					# determine if 1st digit 
+	beq	$t0, $t1, digit2		# branch if 2nd digit 
+
 digit1: 
     li  $t1, 100                # multiply by 100 for 1st digit 
     mult	$s1, $t1 
@@ -32,11 +35,12 @@ digit2:
 	mflo	$t1 
 	add 	$s2, $s2, $t1		# combine 1st digit and 2nd digit 
 
-
     j waiting 					# continue loop 
 
 transmit: 
     add 	$a0, $s2, $s1 		# combine all 3 digits 
 	li 	$v0, 1 					# print decimal (integer) 
+    syscall
 
 	li 	$v0, 10					# end program  
+    syscall
