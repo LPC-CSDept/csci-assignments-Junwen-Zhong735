305b7ecace4ef46ad7cfb39c5b4fa2f1e4ccbec4
Junwen-Zhong735 <94938529+Junwen-Zhong735@users.noreply.github.com>
2021-12-17 16:39:25 -0800

    update

--- Finals/final2.asm
+++ Finals/final2.asm
@@ -1,3 +1,11 @@
+
+	.kdata 
+data1:	.word, 1
+data2:	.word, 2
+	
+	.text
+	.globl main 
+	
 main: 
 	mfc0	$s0, $12 			# read status register
 	ori	$s0, $s0, 0xff11		# enable all interrupts 
@@ -6,3 +14,10 @@ main:
 	lui 	$t0, 0xffff 		# $t0 = 0xffff0000, memory address of reciever control register 
 	ori 	$s0, 2				# enable keyboard interrupts 
 	sw 	$s0, 0($t0) 			# update reciever control register
+
+loop: 
+	j	loop 					# endless loop 
+	
+exit: 
+	li 	$v0, 10 				# end program 
+	syscall 
