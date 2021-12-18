4b919429753b93e68de868a885498d1af74173da
Junwen-Zhong735 <94938529+Junwen-Zhong735@users.noreply.github.com>
2021-12-17 16:55:24 -0800

    update

--- Finals/final2.asm
+++ Finals/final2.asm
@@ -33,6 +33,20 @@ exit:
 	andi	$a0, $a0, 0x1f  	# Get the exception code     
 	bne	$a0, $zero, kdone   	# Exception Code 0 is I/O.
 
+    lui	$k0, 0xffff    			# $v0 = 0xffff0000     
+	lbu	$a0, 4($k0)   			# get the input key 
+	
+	li 	$v0, 113				# $v0 = q, ascii code for q is 113
+	beq 	$v0, $a0, kend 		# branch if input is 'q'
+	
+	sb 	$a0, 12($k0) 			# print the char 
+	
+	j 	kdone 					# prevents program from ending if input isn't 'q'
+	
+kend: 
+	li 	$v0, 10 				# end program if input is 'q'
+	syscall
+
 kdone:     
 	lw	$v0, data1     			# Restore saved registers     
 	lw	$a0, data2     
